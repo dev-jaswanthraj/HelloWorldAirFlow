8ed44e334cb824aba45faa7a16b99d965b084ef0
jaswanthraj R <jaswanthraj.r@gmail.com>
2023-08-22 16:58:18 +0530

    Added 2rd

--- dags/first_dad.py
+++ dags/first_dad.py
@@ -9,9 +9,14 @@ except ImportError as ie:
     print("Error {}!!!!".format(ie))
 
 
-def first_function_execute():
-    print("Hello World")
-    return "Hello World"
+def first_function_execute(**context):
+    print("First Function Execute.....")
+    print(type(context['ti']), context['ti'], dir(context['ti']), sep = "\n****************")
+    context['ti'].xcom_push(key = "name", value = "Jaswanthraj R")
+
+def second_function_execute(**context):
+    instance = context['ti'].xcom_pull(key = "name")
+    print("I am in Second Function Execute got value: {} from function 1......".format(instance))
 
 
 with DAG(
@@ -28,5 +33,13 @@ with DAG(
     first_function_execute = PythonOperator(
         task_id = "first_function_execute",
         python_callable = first_function_execute,
+        provide_context=True,
+        
+    )
+    second_function_execute = PythonOperator(
+        task_id = "second_function_execute",
+        python_callable = second_function_execute,
+        provide_context=True,
     )
     
+first_function_execute >> second_function_execute
